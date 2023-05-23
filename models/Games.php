87a8792b16bb45ccc4c639052d8ded54d5aead57
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-24 00:16:16 +0200

    changed: getMaterial output

--- models/Games.php
+++ models/Games.php
@@ -1,18 +1,22 @@
 <?php 
 
   class Games extends Product{
-    private $material;
+    private $materials;
 
     function __construct($_name, $_type){
       parent::__construct($_name, $_type);
     }
 
-    public function setMaterial($_material){
-      $this->material = $_material;
+    public function setMaterial($_materials){
+      $this->materials = $_materials;
     }
 
     public function getMaterial(){
-      return $this->material;
+      $string = "";
+      foreach ($this->materials as $index => $material){
+        $index < (count($this->materials) - 1) ? $string .= $material . ', ' : $string .= $material . ' ';
+      }
+      return $string;
     }
   }
 ?>
