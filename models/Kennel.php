01dd66a4fe5d054641ef20f30d9895ee054e3cd1
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-23 23:23:59 +0200

    add: Kennel class

---
+++ models/Kennel.php
@@ -0,0 +1,32 @@
+<?php 
+
+  class Kennel extends Product{
+    private $isForOutside;
+    private $materials;
+
+    function __construct($_name, $_type){
+      parent::__construct($_name, $_type);
+    }
+
+    public function setOutside($_isForOutside){
+      $this->isForOutside = $_isForOutside;
+    }
+
+    public function getOutside(){
+      $this->isForOutside ? $string = 'Adatta per esterni' : $string = 'Non adatta per esetrni';
+      return $string;
+    }
+
+    public function setMaterial($_materials){
+      $this->materials = $_materials;
+    }
+
+    public function getMaterial(){
+      $string = "";
+      foreach ($this->materials as $index => $material){
+        $index < (count($this->materials) - 1) ? $string .= $material . ', ' : $string .= $material . ' ';
+      }
+      return $string;
+    }
+  }
+?>
