a5ae8db95abef067116979815f90ad9db845439e
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-23 23:25:10 +0200

    add: foreach in body

--- Views/layouts/body.php
+++ Views/layouts/body.php
@@ -1,14 +1,17 @@
   <body>
-
     <main>
-      <div class="container">
-        <?php 
-        
-          foreach($arrayProducts as $product){
-            var_dump($product);
-          }
-        
-        ?>
+      <div class="pg-wrapper vh-100 w-100 bg-dark">
+        <div class="container d-flex py-5">
+          <div class="row row-cols-4 w-100">
+            <?php 
+            
+              foreach($arrayProducts as $product){
+                include __DIR__ . "/card.php";
+              }
+            
+            ?>
+          </div>
+        </div>
       </div>
     </main>
   </body>
