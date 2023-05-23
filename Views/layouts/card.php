734916a4e236f4ce7ef15eadad4ebfffecc6d8b7
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-23 23:25:20 +0200

    add: card output

--- Views/layouts/card.php
+++ Views/layouts/card.php
@@ -1,7 +1,20 @@
-<div class="card mx-3" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"></h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<div class="col shadow-sm">
+  <div class="card w-100 h-100" style="width: 18rem;">
+    <?php echo $product->getImage(); ?>
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title"><?php echo $product->getName(); ?></h5>
+      <span class="card-text">Brand: <?php echo $product->getBrand(); ?></span>
+      <span class="card-text">Prezzo: <?php echo $product->getPrice(); ?></span>
+      <span class="card-text"><?php echo $product->getAnimal(); ?></span>
+
+      <?php if($product->type === 'food'){ ?>
+        <span class="card-text"><span class="fs-5">Ingredienti:</span> <?php echo $product->getIngrediets(); ?></span>
+      <?php }elseif($product->type === 'games'){ ?>
+        <span class="card-text"><span class="fs-5">Materiali:</span> <?php echo $product->getMaterial(); ?></span>
+        <?php }elseif($product->type === 'kennel'){ ?>
+          <span class="card-text"><?php echo $product->getOutside(); ?></span>
+        <span class="card-text"><span class="fs-5">Lista dei materiali:</span> <?php echo $product->getMaterial(); ?></span>
+      <?php }?> 
+    </div>
   </div>
 </div>
